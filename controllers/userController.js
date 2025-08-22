0a5981089e9fafd6d547d6946cd96f265a5e680b
Sankhani-Wanninayake <183672050+Sankhani-Wanninayake@users.noreply.github.com>
2025-08-22 13:06:51 +0530

    Improve admin validation messages in user creation and product creation functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,43 +2,39 @@ import Users from '../models/user.js';
 
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
-import dotenv from "dotenv";
-dotenv.config();
 
 export function createUser(req, res) {
+    const newUserData = req.body;
 
-    const newUserData = req.body
-if (newUserData.type == null){
-    res.json({
-        message: "please login as administrator to create admin account "
-    })
-    return
-}
-    if (newUserData.type != "admin") {
-        res.json(
-            {
-                message: "you are not allowed to create user account as you are not admin"
-            })
-        return
-     }
-
+    if (newUserData.type == "admin") {
+        if (req.user == null) {
+            res.json({
+                message: "please login as administrator to create admin account"
+            });
+            return;
+        }
     }
 
-
-    newUserData.password = bcrypt.hashSync(newUserData.password, 10)
-
-    const user = new Users(newUserData)
-    user.save().then(() => {
+    if (req.user && req.user.type != "admin" && newUserData.type == "admin") {
         res.json({
-            message: "user created"
-        })
-    }).catch((e) => {
-console.log(e);
-        res.status(500).json({
-            message: "user not created"
+            message: "you are not allowed to create an admin account"
+        });
+        return;
+    }
 
+    
+    newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+
+    const user = new Users(newUserData);
+    user.save()
+        .then(() => {
+            res.json({ message: "user created" });
         })
-    })
+        .catch((e) => {
+            console.log(e);
+            res.status(500).json({ message: "user not created" });
+        });
+}
 
 
 
